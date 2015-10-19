b7cb30521bef20bbd56914f65e837993982f2f67
FionaAndJohn <FionaAndJohn@apple.com>
2015-10-19 14:38:52 -0400

    Add User controls.

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -5,16 +5,68 @@ url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users/5.json',
-  query_values: {
-    'some_category[a_key]' => 'another value',
-    'some_category[a_second_key]' => 'yet another value',
-    'some_category[innter_inner[key]]' => 'value',
-    'something_else' => 'ahhahahahah'
-  }
-).to_s
+  path: '/users.json',
+  ).to_s
 
 puts RestClient.get(url)
-puts RestClient.get(url)
 
-puts RestCliemt.post(url)
+puts
+puts
+
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(
+    url,
+    { user: { name: "Gizmo", email: "gizmo@gizmo.gizmo" } }
+  )
+end
+
+# create_user
+
+def destroy_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/4.json'
+  ).to_s
+
+  puts RestClient.delete(
+    url,
+    { user: { name: "Gizmo", email: "gizmo@gizmo.gizmo" } }
+  )
+end
+
+def update_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/3.json'
+  ).to_s
+
+  puts RestClient.patch(
+    url,
+    { user: { name: "Ben", email: "BennyBennassi@thebiz.com" } }
+  )
+end
+
+def show_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/2.json',
+    ).to_s
+
+  puts RestClient.get(url)
+end
+
+show_user
+# update_user
